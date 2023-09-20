a8f919bbe121d1d0fdb39b0c3981ff47855e3fab
Ali <stealth4119@gmail.com>
2023-09-20 15:23:34 +0300

    обновление курсов

--- models/Course.model.js
+++ models/Course.model.js
@@ -1,22 +1,34 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const courseSchema = mongoose.Schema({
-    title: String,
-    text: String,
-    materials: {
-        url: [{
-            ref: String
-        }],
-        video: [{
-            vid: String
-        }],
-        task: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: 'Task'
-        }
-    },
-    img: String
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  materials: {
+    url: [
+      {
+        ref: String,
+      },
+    ],
+    video: [
+      {
+        vid: String,
+      },
+    ],
+    task: [
+      {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "Task",
+      },
+    ],
+  },
+  img: String,
+});
 
-const Course = mongoose.model('Course', courseSchema)
-module.exports = Course
+const Course = mongoose.model("Course", courseSchema);
+module.exports = Course;
--- routes/course.route.js
+++ routes/course.route.js
@@ -3,10 +3,10 @@ const { courseController} = require('../controllers/course.controller')
 // const authMiddleware = require('../middlewares/auth.middleware')
 const router = Router()
 
-router.post('/news', courseController.postCourse)
-router.delete('/news/:id', courseController.deleteCourseId)
-router.patch('/news/:id', courseController.patchCourseId)
-router.get('/news/:id', courseController.getCourseId)
-router.get('/news', courseController.getCourse)
+router.post('/course', courseController.postCourse)
+router.delete('/course/:id', courseController.deleteCourseId)
+router.patch('/course/:id', courseController.patchCourseId)
+router.get('/course/:id', courseController.getCourseId)
+router.get('/course', courseController.getCourse)
 
 module.exports = router 
