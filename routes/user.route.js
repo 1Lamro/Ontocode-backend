82f76d434966d2687ddd94345de34d537063d79e
Mokhamed <gari142@mail.ru>
2023-09-25 15:22:54 +0300

    Доработал

--- routes/user.route.js
+++ routes/user.route.js
@@ -17,7 +17,7 @@ router.get("/allimages", checkAuth, userController.findImages); // вывод ф
 router.patch("/editimage", upload.single("img"), checkAuth, userController.editImage); // изменение авы
 router.post('/login', loginValidator,  userController.login);
 router.get('/profile', userController.getUserProfile);
-router.put('/profile/avatar', userController.updateAvatar);
-router.patch('/course/:userId', userController.updateCourse)
+// router.put('/profile/avatar', userController.updateAvatar);
+ router.patch('/course/:userId', userController.updateCourse)
 
 module.exports = router;
