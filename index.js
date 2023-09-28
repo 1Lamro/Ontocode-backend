ca1650ebccbb663645a46391b9dd9b7237598f5d
Ali <stealth4119@gmail.com>
2023-09-29 00:40:16 +0300

    минимальные фиксы

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const http = require('http').Server(app)
 
 const socketIO = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:5174'
+    origin: 'http://localhost:5173'
   }
 })
 
